7a92bc9e030f56339fdd82231664d5b5063d7a36
Eugene Baklanov <miltenfiremage@gmail.com>
2017-08-20 00:55:33 +0600

    Changing build system to CMake.
    Still not sure how exactly the whole thing should work with git, though.

---
+++ include/render.h
@@ -0,0 +1,10 @@
+#ifndef __TT_RENDER_INCLUDED__
+#define __TT_RENDER_INCLUDED__
+#include <cstdint>
+#include <ios>
+
+void img_over_video_thread (uint8_t*, uint8_t*, uint8_t*, uint8_t*, std::streamsize, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+void img_over_video (uint8_t*, uint8_t*, uint8_t*, uint8_t*, std::streamsize, unsigned int, unsigned int, unsigned int, unsigned int);
+void img_over_frame (uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, unsigned int, unsigned int, unsigned int, unsigned int);
+
+#endif
