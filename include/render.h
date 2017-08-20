0652807c835affd9952f669719cef9a054d1efac
Eugene Baklanov <miltenfiremage@gmail.com>
2017-08-20 21:29:50 +0600

    Trying to do the second bonus task
    A few first drafts of new functions.

--- include/render.h
+++ include/render.h
@@ -2,9 +2,13 @@
 #define __TT_RENDER_INCLUDED__
 #include <cstdint>
 #include <ios>
+#include <GLFW/glfw3.h>
 
 void img_over_video_thread (uint8_t*, uint8_t*, uint8_t*, uint8_t*, std::streamsize, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
 void img_over_video (uint8_t*, uint8_t*, uint8_t*, uint8_t*, std::streamsize, unsigned int, unsigned int, unsigned int, unsigned int);
 void img_over_frame (uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, unsigned int, unsigned int, unsigned int, unsigned int);
+void render_video(uint8_t*, std::streamsize, unsigned int, unsigned int, unsigned int);
+//void render_frame(GLFWwindow*, uint8_t*, uint8_t*, uint8_t*, unsigned int, unsigned int);
+void render_window_input(GLFWwindow*);
 
 #endif
--- render.cpp
+++ render.cpp
@@ -1,4 +1,8 @@
 #include <cmath>
+#include <chrono>
+#include <thread>
+#include <GLEW/gl.h>
+#include <GLFW/glfw3.h>
 #include "render.h"
 
 //Overlays image over video, supposed to be spawned in a thread.
@@ -89,5 +93,127 @@ void img_over_frame (uint8_t* v_ybuffer, uint8_t* v_ubuffer, uint8_t* v_vbuffer,
             abspos++;
         }
         abspos+=wdiff;
+    }
+}
+
+void render_video(uint8_t* vbuffer, std::streamsize size, unsigned int framerate, unsigned int width, unsigned int height)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+    glfwWindowHint(GLFW_WINDOW_RESIZABLE,GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "YUV video render", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    std::thread ithread = std::thread(render_window_input,window);
+
+    const GLchar* vertex_code = 
+        R"glsl(
+        #version 150 core
+        in vec2 position;
+        in vec2 texcoord;
+        out vec2 Texcoord;
+        void main()
+        {
+            Texcoord = texcoord;
+            gl_Position = vec4(position,0.0,1.0);
+        }
+        )glsl";
+    const GLchar* fragment_code = 
+        R"glsl(
+        #version 150 core
+        in vec2 Texcoord;
+        out vec4 endcolor;
+        uniform sampler2D ybuffer;
+        uniform sampler2D ubuffer;
+        uniform sampler2D vbuffer;
+        void main()
+        {
+        }
+        )glsl";
+
+    unsigned long long framelen = width*height+((width*height)/2);
+    unsigned long frames = size/framelen;
+    uint8_t *yholder,*uholder,*vholder;
+
+    GLuint vao;
+    glGenVertexArrays(1,&vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo;
+    glGenBuffers(1,$vbo);
+
+    //Rectangle vertices
+    GLfloat vertices[] = {
+        //  Position        Texcoords
+            -1.0f,  1.0f,   0.0f,   0.0f,   //Top-left corner
+            1.0f,   1.0f,   1.0f,   0.0f,   //Top-right corner
+            1.0f,   -1.0f,  1.0f,   1.0f,   //Bottom-right corner
+            -1.0f,  -1.0f,  0.0f,   1.0f,   //Bottom-left corner
+    }
+
+    glBindBuffer(GL_ARRAY_BUFFER,vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vshader,1,&vertex_code,NULL);
+    glCompileShader(vshader);
+
+    GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fshader,1,&fragment_code,NULL);
+    glCompileShader(fshader);
+
+    GLuint sprogram = glCreateProgram();
+    glAttachShader(sprogram,vshader);
+    glAttachShader(sprogram,fshader);
+    glBindFragDataLocation(sprogram,0,"endcolor");
+    glLinkProgram(sprogram);
+    glUseProgram(sprogram);
+
+    GLint posattr = glGetAttribLocation(sprogram,"position");
+    glEnableVertexAttribArray(posattr);
+    glVertexAttribPointer(posattr,2,GL_FLOAT,GL_FALSE,4*sizeof(GLfloat),0);
+
+    GLint texattr = glGetAttribLocation(sprogram,"texcoord");
+    glEnableVertexAttribArray(texattr);
+    glVertexAttribPointer(texattr,2,GL_FLOAT,GL_FALSE,4*sizeof(GLfloat),2*sizeof(GLfloat));
+
+    for (unsigned long i=0;i<frames;i++)
+    {
+        double start_frame = glfwGetTime();
+        glfwSwapBuffers(window);
+        yholder=&vbuffer[framelen*i];
+        uholder=yholder+(width*height);
+        vholder=uholder+((width*height)/4);
+        //render_frame(window,yholder,uholder,vholder,width,height);
+        glFlush();
+        double finished_frame = glfwGetTime();
+        double elapsed_time = finished_time-start_time;
+        if (elapsed_time<(1.0/framerate))
+            std::this_thread::sleep_for(std::chrono::duration<double>((1.0/framerate)-elapsed_time));
+    }
+
+    glfwWindowShouldClose(window,GL_TRUE);
+    ithread.join();
+    glfwTerminate();
+}
+
+//void render_frame(GLFWwindow* window, uint8_t* ybuffer, uint8_t* ubuffer, uint8_t* vbuffer, unsigned int width, unsigned int height)
+//{
+//}
+
+void render_window_input(GLFWwindow* window)
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwWaitEvents();
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            break;
     }
 }
